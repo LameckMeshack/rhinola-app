d67bc9f6ab64c534f5a0ec6b22c39dc5c66ecffb
RhinolamerSonwar <rhinolamer@gmail.com>
2021-11-16 07:20:47 +0300

    shared code on github first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,59 @@
+import data from "./data";
+function App() {
+  return (
+    <div classNameName="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="/">
+            rhinola
+          </a>
+        </div>
+        <div>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => (
+            <div key={product._id} className="card">
+              <a href={`/product/${product._id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
+              </a>
+              <div className="card-body">
+                <a href={`/product/${product._id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">${product.price}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
+      <footer className="row center">All Right Reserve Rhinola 2021</footer>
+    </div>
+  );
+}
+
+export default App;
