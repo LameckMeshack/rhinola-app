af6ab1909432d4ea5caa19738a6ebfa610f11340
RhinolamerSonwar <RhinolamerSonwar>
2021-11-20 23:52:34 +0300

    redux added to the homescreen

---
+++ frontend/src/store.js
@@ -0,0 +1,18 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import { productListReducer } from "./reducers/productReducers";
+
+const initialState = {};
+const reducer = combineReducers({
+  productList: productListReducer,
+});
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store;
