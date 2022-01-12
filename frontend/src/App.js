1f4ac6995403822b9a542977635adecb8d3f24cd
RhinolamerSonwar <RhinolamerSonwar>
2022-01-12 18:01:05 +0300

    implemented sign in screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import ProductScreen from "./screens/ProductScreen";
 import HomeScreen from "./screens/HomeScreen";
 import CartScreen from "./screens/CartScreen";
 import { useSelector } from "react-redux";
+import SigninScreen from "./screens/SigninScreen";
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
@@ -30,6 +31,7 @@ function App() {
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
 
