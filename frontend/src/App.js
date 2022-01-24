384897bf0a56547e00090a90acab8543d10256aa
Rhinolamer Sonwar <rhinolamerSonwar>
2022-01-24 12:08:17 +0300

    created register screem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import CartScreen from "./screens/CartScreen";
 import { useDispatch, useSelector } from "react-redux";
 import SigninScreen from "./screens/SigninScreen";
 import { signout } from "./actions/userActions";
+import RegisterScreen from "./screens/RegisterScreen";
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
@@ -53,6 +54,7 @@ function App() {
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
 
--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -65,7 +65,10 @@ function SigninScreen(props) {
           <div>
             <label />
             <div>
-              New customer? <Link to="/register">Create your account</Link>
+              New customer?{" "}
+              <Link to={`/register?redirect=${redirect}`}>
+                Create your account
+              </Link>
             </div>
           </div>
         </form>
