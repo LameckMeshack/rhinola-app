2783658976f0e14162f26fcee7ad0bace4650536
Rhinolamer Sonwar <rhinolamerSonwar>
2022-01-26 18:44:19 +0300

    added paypal button

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,9 @@ app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
+app.get("/api/config/paypal", (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+});
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
