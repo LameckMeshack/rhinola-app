5606d94abca6da0eeafebb640caead7cab3c90c3
Rhinolamer Sonwar <rhinolamerSonwar>
2022-02-21 14:31:29 +0300

    Created Admin Links

--- backend/utils.js
+++ backend/utils.js
@@ -38,3 +38,11 @@ export const isAuth = (req, res, next) => {
     res.status(401).send({ message: "No Token" });
   }
 };
+
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: "Invlaid Admin Token" });
+  }
+};
