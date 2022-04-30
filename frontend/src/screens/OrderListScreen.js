12631eabfd7b0d063ce6ee64b2605f6be9e12a77
Rhinolamer Sonwar <rhinolamerSonwar>
2022-04-30 11:02:00 +0300

    everything is ok

--- frontend/src/screens/OrderListScreen.js
+++ frontend/src/screens/OrderListScreen.js
@@ -54,6 +54,7 @@ export default function OrderListScreen(props) {
             {orders.map((order) => (
               <tr key={order._id}>
                 <td>{order._id}</td>
+
                 <td>{order.user.name}</td>
                 <td>{order.createdAt.substring(0, 10)}</td>
 
